3a10fe1790905c855b6b35953d38847045abaf01
Andria Begiashvili <92532950+AndriaBegiashvili@users.noreply.github.com>
2022-12-03 00:15:46 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('secure') -> get('/', function () {
+Route::get('/', function () {
     return "home page";
 });
 Route:: get('/error', function () {
